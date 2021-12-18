0f94b011190396186bf5de710d72f358cbb88b4c
Jakub Sieczczyński <j.sieczczynski@gmail.com>
2021-12-18 12:11:48 +0100

    GW-add_eslint_prettier_jwt_and_rate_limiting

---
+++ src/controllers/authController.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/user'
+
+export default {
+    async login(req, res, next) {
+        const token = jwt.sign({ id: req.user._id }, process.env.JWT_TOKEN, {
+            expiresIn: 60 * 60,
+        })
+        return res.send({ token })
+    },
+    async register(req, res, next) {
+        const { login, password } = req.body
+        const user = new User({ login })
+        await User.register(user, password)
+
+        res.send('User created!')
+    },
+}
